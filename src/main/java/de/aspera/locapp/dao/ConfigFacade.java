8ce92c440d0bfb0c9189d897ff056915a3ecc687
geramaya <geramaya@gmail.com>
2019-02-26 11:21:32 +0100

    change for openjdk 11 (#1)
    * Set theme jekyll-theme-cayman
    
    * NEW: compile & run with openjdk 11

--- src/main/java/de/aspera/locapp/dao/ConfigFacade.java
+++ src/main/java/de/aspera/locapp/dao/ConfigFacade.java
@@ -16,38 +16,43 @@ import de.aspera.locapp.util.ValidationHelper;
  */
 public class ConfigFacade extends AbstractFacade<Config> {
 
-    private static final Logger LOGGER = Logger.getLogger(ConfigFacade.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(ConfigFacade.class.getName());
 
-    public ConfigFacade() {
-        super(Config.class);
-    }
+	public ConfigFacade() {
+		super(Config.class);
+	}
 
-    public String[] getValue(String key) throws DatabaseException {
-        getEntityManager().getTransaction().begin();
-        String queryStr = "select target.value from " + Config.class.getSimpleName() + " target where target.key =:key";
-        Query query = getEntityManager().createQuery(queryStr);
-        query.setParameter("key", key);
-        String value = (String) query.getSingleResult();
-        String[] values = value.split(",");
-        getEntityManager().getTransaction().commit();
-        return values;
-    }
+	public String[] getValue(String key) throws DatabaseException {
 
-    public void saveConfig(List<Config> configs) throws DatabaseException {
-        try {
-            getEntityManager().getTransaction().begin();
-            for (Config config : configs) {
-                ValidationHelper.validateBean(config);
-                getEntityManager().persist(config);
-            }
-            getEntityManager().getTransaction().commit();
+		String[] values = new String[] {};
+		getEntityManager().getTransaction().begin();
+		String queryStr = "select target.value from " + Config.class.getSimpleName() + " target where target.key =:key";
+		Query query = getEntityManager().createQuery(queryStr);
+		query.setParameter("key", key);
+		@SuppressWarnings("rawtypes")
+		List resultList = query.getResultList();
+		if (resultList != null && resultList.size() > 0)
+			values = resultList.get(0).toString().split(",");
+		// String value = (String) query.getSingleResult();
+		getEntityManager().getTransaction().commit();
+		return values;
+	}
 
-        } catch (Exception e) {
-            getLogger().log(Level.SEVERE, e.getMessage(), e);
-            if (getEntityManager().getTransaction().isActive()) {
-                getEntityManager().getTransaction().setRollbackOnly();
-            }
-            throw new DatabaseException(e.getMessage(), e);
-        }
-    }
+	public void saveConfig(List<Config> configs) throws DatabaseException {
+		try {
+			getEntityManager().getTransaction().begin();
+			for (Config config : configs) {
+				ValidationHelper.validateBean(config);
+				getEntityManager().persist(config);
+			}
+			getEntityManager().getTransaction().commit();
+
+		} catch (Exception e) {
+			getLogger().log(Level.SEVERE, e.getMessage(), e);
+			if (getEntityManager().getTransaction().isActive()) {
+				getEntityManager().getTransaction().setRollbackOnly();
+			}
+			throw new DatabaseException(e.getMessage(), e);
+		}
+	}
 }
