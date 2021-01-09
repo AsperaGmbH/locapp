53fa405a816a857764f824c08e778908432eb341
geramaya <geramaya@gmail.com>
2021-01-09 02:03:20 +0100

    REF: use FileUtils and improve error handling

--- src/main/java/de/aspera/locapp/cmd/ImportIgnoredItemsCommand.java
+++ src/main/java/de/aspera/locapp/cmd/ImportIgnoredItemsCommand.java
@@ -1,80 +1,71 @@
 package de.aspera.locapp.cmd;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
+
+import org.apache.commons.io.FileUtils;
 
 import de.aspera.locapp.dao.DatabaseException;
 import de.aspera.locapp.dao.IgnoredItemFacade;
 import de.aspera.locapp.dto.IgnoredItem;
 
 public class ImportIgnoredItemsCommand implements CommandRunnable {
-    private static final Logger logger = Logger.getLogger(ImportIgnoredItemsCommand.class.getName());
+	private static final Logger logger = Logger.getLogger(ImportIgnoredItemsCommand.class.getName());
 
-    private IgnoredItemFacade ignoredItemFacade = new IgnoredItemFacade();
+	private IgnoredItemFacade ignoredItemFacade = new IgnoredItemFacade();
 
-    @Override
-    public void run() {
-        var ctx = CommandContext.getInstance();
-        var ignoredItemsListPath = ctx.nextArgument();
-        var ignoredItems = readIgnoredItemsFile(ignoredItemsListPath);
+	@Override
+	public void run() {
+		var ctx = CommandContext.getInstance();
+		var ignoredItemsListPath = ctx.nextArgument();
+		var ignoredItems = readIgnoredItemsFile(ignoredItemsListPath);
+		try {
+			for (var ignoredItem : ignoredItems) {
+				ignoredItemFacade.create(ignoredItem);
+				System.out.println(" >> Add ignore entry: " + ignoredItem.getFileName() + " to be ignored on export. <<");
+			}
+			if (ignoredItems != null && ignoredItems.size() >= 0)
+				logger.log(Level.INFO, "Marked " + ignoredItems.size() + " files to be ignored.");
+		} catch (DatabaseException e) {
+			logger.log(Level.SEVERE, "Error while saving IgnoredItem entity.", e);
+			return;
+		}
+	}
 
-        try {
-            for (var ignoredItem : ignoredItems) {
-                ignoredItemFacade.create(ignoredItem);
-            }
-        } catch (DatabaseException e) {
-            logger.log(Level.SEVERE, "Error while saving IgnoredItem entity.", e);
-            return;
-        }
+	private List<IgnoredItem> readIgnoredItemsFile(String filePath) {
+		List<IgnoredItem> ignoredItems = new ArrayList<>();
+		final Set<String> linesOfFile;
+		try {
+			linesOfFile = new HashSet<>(FileUtils.readLines(new File(filePath)));
+		} catch (Exception e) {
+			logger.log(Level.WARNING, "File " + filePath + " could not found or was invalid!.");
+			return ignoredItems;
+		}
+		
+		Set<String> savedIgnoreFiles = getSavedIgnoreFiles();
+		for (Iterator<String> iterator = linesOfFile.iterator(); iterator.hasNext();) {
+			String string = (String) iterator.next();
+			if (savedIgnoreFiles.contains(string))
+				continue;
+			var item = new IgnoredItem();
+			item.setFileName(string);
+			ignoredItems.add(item);
+		}
+		return ignoredItems;
+	}
 
-        logger.log(Level.INFO, "Marked " + ignoredItems.size() + " files to be ignored.");
-    }
-
-    private List<IgnoredItem> readIgnoredItemsFile(String filePath) {
-        List<IgnoredItem> ignoredItems = new ArrayList<>();
-        File file;
-        FileInputStream inputStream;
-
-        try {
-            file = new File(filePath);
-            inputStream = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            logger.log(Level.SEVERE, "File " + filePath + " not found.", e);
-            return ignoredItems;
-        }
-         
-        var streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-        var fileAsText = new BufferedReader(streamReader)
-            .lines()
-            .collect(Collectors.joining("\n"));
-
-        try {
-            streamReader.close();
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Error while closing stream reader.", e);
-            return ignoredItems;
-        }
-        
-        StringTokenizer tokenizer = new StringTokenizer(fileAsText);
-        
-        while (tokenizer.hasMoreTokens()) {
-            var token = tokenizer.nextToken();
-            var item = new IgnoredItem();
-            item.setFileName(token);
-            ignoredItems.add(item);
-        }
-
-        return ignoredItems;
-    }
+	private Set<String> getSavedIgnoreFiles() {
+		List<IgnoredItem> ignored = ignoredItemFacade.findAll();
+		Set<String> knownIgnoreFiles = new HashSet<>();
+		for (IgnoredItem ignoreItem : ignored) {
+			knownIgnoreFiles.add(ignoreItem.getFileName());
+		}
+		return knownIgnoreFiles;
+	}
 }
